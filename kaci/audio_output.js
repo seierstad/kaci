4b2f0b532c043c058e379c27a36aa6758e61101d
Erik Erik Seierstad <erik.seierstad@gmail.com>
2012-04-18 22:43:18 +0200

    webkitAudioContext support (not tested)

--- kaci/audio_output.js
+++ kaci/audio_output.js
@@ -17,57 +17,86 @@ var kaci = kaci || {};
             disable,
             timerID,
             setReadFunction,
-            readFunction;
-            
-        audioElement.mozSetup(1, synth.sampleRate);
-        
-        getAudio = function() {
-    		var written, soundData, currentPosition, available;
+            readFunction,
+            context,
+            audioNode;
 
-		    if (tail) {  
-			    written = audioElement.mozWriteAudio(tail);
-			    currentWritePosition += written;
-			    if (written < tail.length) {
-				    // Not all the data was written, saving the tail...
-				    tail = (tail.subarray ? tail.subarray(written) : tail.slice(written));
-				    return; // ... and exit the function.
-			    }
-			    tail = null;
-		    }
-		    currentPosition = audioElement.mozCurrentSampleOffset();
-		    available = currentPosition + prebufferSize - currentWritePosition;
-		    
-		    if (available > 0) {
-			    // Request some sound data from the callback function.
-			    soundData = new Float32Array(parseFloat(available));
-			    readFunction(soundData);
-
-			    // Writing the data.
-			    written = audioElement.mozWriteAudio(soundData);
-			    if (written < soundData.length) {
-				    // Not all the data was written, saving the tail.
-				    tail = (soundData.subarray ? soundData.subarray(written) : soundData.slice(written));
-			    }
-			    currentWritePosition += written;
-		    }
-		};
-        enable = function() {
-            timerID = setInterval(getAudio, 100);
-        };
-        disable = function() {
-            clearInterval(timerID);
-        };
         setReadFunction = function(fn) {
             if (typeof fn === 'function') {
                 readFunction = fn;
             }
         };
-        return {
-            enable: enable,
-            disable: disable,
-            setReadFunction: setReadFunction,
-            readFunction: readFunction
-        };        
+            
+        if (typeof audioElement.mozSetup === 'function') {
+            audioElement.mozSetup(1, synth.sampleRate);
+            
+            getAudio = function() {
+        		var written, soundData, currentPosition, available;
+
+		        if (tail) {  
+			        written = audioElement.mozWriteAudio(tail);
+			        currentWritePosition += written;
+			        if (written < tail.length) {
+				        // Not all the data was written, saving the tail...
+				        tail = (tail.subarray ? tail.subarray(written) : tail.slice(written));
+				        return; // ... and exit the function.
+			        }
+			        tail = null;
+		        }
+		        currentPosition = audioElement.mozCurrentSampleOffset();
+		        available = currentPosition + prebufferSize - currentWritePosition;
+		        
+		        if (available > 0) {
+			        // Request some sound data from the callback function.
+			        soundData = new Float32Array(parseFloat(available));
+			        readFunction(soundData);
+
+			        // Writing the data.
+			        written = audioElement.mozWriteAudio(soundData);
+			        if (written < soundData.length) {
+				        // Not all the data was written, saving the tail.
+				        tail = (soundData.subarray ? soundData.subarray(written) : soundData.slice(written));
+			        }
+			        currentWritePosition += written;
+		        }
+		    };
+            enable = function() {
+                timerID = setInterval(getAudio, 100);
+            };
+            disable = function() {
+                clearInterval(timerID);
+            };
+            return {
+                enable: enable,
+                disable: disable,
+                setReadFunction: setReadFunction,
+                readFunction: readFunction
+            };
+        } else if (typeof webkitAudioContext === 'function') {
+            console.log('web audio api supported');
+            
+            context = new webkitAudioContext();
+            audioNode = context.createJavaScriptNode(128, 1, 1);
+
+            enable = function () {
+                audioNode.connect(context.destination);
+            };
+
+            disable = function () {
+                audioNode.disconnect();
+            };
+
+            audioNode.onaudioprocess = function(event) {
+                var right, left, bufferLength, i;
+                right = event.outputBuffer.getChannelData(0);
+                readFunction(right);                
+            }; 
+            return {
+                enable: enable,
+                disable: disable,
+                setReadFunction: setReadFunction
+            }
+        }
     })();
 
     if (!synth.audioOutput) {
