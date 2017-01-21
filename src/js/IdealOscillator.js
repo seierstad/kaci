021fba3ec36a2cd600c344b63f485f1f6d267d61
Erik E. Seierstad <erik.seierstad@gmail.com>
2017-01-21 19:57:19 +0100

    IdealOscillator -> es2016 class

--- src/js/IdealOscillator.js
+++ src/js/IdealOscillator.js
@@ -1,161 +1,171 @@
-/*globals require, module, CustomEvent */
-"use strict";
 import DCGenerator from "./DCGenerator";
 import {BUFFER_LENGTH} from "./constants";
-
 import {waveforms} from "./waveforms";
+import {PannableModule} from "./sharedFunctions";
 
-let IdealOscillator = function (context) {
-    let i,
-        j,
-        def,
-        key,
-        zeroPhaseActions;
 
-    this.inputDefs = [{
-        name: "frequency",
-        defaultValue: 440
-    }, {
-        name: "detune",
-        defaultValue: 0
-    }];
-    this.context = context;
-    this.phase = 0;
-    this.paused = false;
-    this.sampleAndHoldBuffer = {
-        value: null,
-        phase: 0
-    };
-    this.dc = new DCGenerator(context);
-    this.mergedInput = context.createChannelMerger(this.inputDefs.length);
+class IdealOscillator extends PannableModule {
+    constructor (context, dc) {
+        super();
 
-    for (i = 0, j = this.inputDefs.length; i < j; i += 1) {
-        def = this.inputDefs[i];
+        this.context = context;
 
-        this[def.name + "Node"] = context.createGain();
-        this.dc.connect(this[def.name + "Node"]);
-        this[def.name] = this[def.name + "Node"].gain;
-        this[def.name].value = def.defaultValue;
-        this[def.name + "Node"].connect(this.mergedInput, null, i);
+        this.inputDefs = [{
+            name: "frequency",
+            defaultValue: 10
+        }, {
+            name: "detune",
+            defaultValue: 0
+        }];
+
+        this.phase = 0;
+        this.paused = false;
+
+        this.sampleAndHoldBuffer = {
+            value: null,
+            phase: 0
+        };
+
+        this.dc = dc || new DCGenerator(context);
+        this.mergedInput = context.createChannelMerger(this.inputDefs.length);
+
+        this.inputDefs.forEach((def, i) => {
+            const nodeName = def.name + "Node";
+
+            this[nodeName] = context.createGain();
+            this.dc.connect(this[nodeName]);
+            this[def.name] = this[nodeName].gain;
+            this[def.name].value = def.defaultValue;
+            this[nodeName].connect(this.mergedInput, null, i);
+        });
+
+        this.generator = context.createScriptProcessor(BUFFER_LENGTH, this.inputDefs.length, 1);
+        this.mergedInput.connect(this.generator);
+
+        this.getIncrementedPhase = this.getIncrementedPhase.bind(this);
+
+        this.getAudioProcessHandler = this.getAudioProcessHandler.bind(this);
+        this.handleAudioProcess = this.getAudioProcessHandler();
     }
-    this.generator = context.createScriptProcessor(BUFFER_LENGTH, this.inputDefs.length, 1);
-    this.mergedInput.connect(this.generator);
 
-};
-IdealOscillator.waveforms = waveforms;
-IdealOscillator.prototype.getGenerator = function (oscillator) {
-    return function audioprocessHandler (evt) {
+    getAudioProcessHandler () {
+        const previous = {
+            "frequency": 0,
+            "detune": 0,
+            "calculatedFrequency": 0
+        };
+        const oscillator = this;
 
-        let frequency = evt.inputBuffer.getChannelData(0),
-            detune = evt.inputBuffer.getChannelData(1),
-            output = evt.outputBuffer.getChannelData(0),
-            i, j,
-            calculatedFrequency,
-            phase,
-            previous = {
-                frequency: 0,
-                detune: 0,
-                calculatedFrequency: 0
-            };
+        return (evt) => {
+            const frequency = evt.inputBuffer.getChannelData(0);
+            const detune = evt.inputBuffer.getChannelData(1);
+            const output = evt.outputBuffer.getChannelData(0);
+            let calculatedFrequency;
 
-        for (i = 0, j = this.bufferSize; i < j; i += 1) {
-            if (frequency[i] === previous.frequency && detune[i] === previous.detune) {
-                calculatedFrequency = previous.calculatedFrequency;
-            } else {
-                calculatedFrequency = oscillator.getComputedFrequency(frequency[i], detune[i]);
-                previous.frequency = frequency[i];
-                previous.detune = detune[i];
-                previous.calculatedFrequency = calculatedFrequency;
+            for (let i = 0; i < BUFFER_LENGTH; i += 1) {
+                if (frequency[i] === previous.frequency && detune[i] === previous.detune) {
+                    calculatedFrequency = previous.calculatedFrequency;
+                } else {
+                    calculatedFrequency = oscillator.getComputedFrequency(frequency[i], detune[i]);
+                    previous.frequency = frequency[i];
+                    previous.detune = detune[i];
+                    previous.calculatedFrequency = calculatedFrequency;
+                }
+                const phase = oscillator.getIncrementedPhase(calculatedFrequency);
+                output[i] = oscillator.selectedWaveform.call(oscillator, phase);
             }
-            phase = oscillator.getIncrementedPhase(calculatedFrequency);
-            output[i] = oscillator.selectedWaveform.call(oscillator, phase);
-        }
-    };
-};
-IdealOscillator.prototype.getIncrementedPhase = function getIncrementedPhase (frequency) {
-    let increment = frequency / this.context.sampleRate;
-    this.phase += increment;
-    if (this.phase > 1) {
-        while (this.phase > 1) {
-            this.phase -= 1;
-        }
-        this.zeroPhaseActions();
+        };
     }
-    return this.phase;
-};
-IdealOscillator.prototype.requestZeroPhaseEvent = function requestZeroPhaseEvent (eventName) {
 
-    if (!this.zeroPhaseEventRequested) {
-        this.zeroPhaseEventRequested = [];
-    }
-    if (this.zeroPhaseEventRequested.indexOf(eventName) === -1) {
-        this.zeroPhaseEventRequested.push(eventName);
-    }
-};
-IdealOscillator.prototype.zeroPhaseActions = function () {
-    let i, j,
-        event,
-        that = this;
-
-    if (this.zeroPhaseEventRequested) {
-        for (i = 0, j = this.zeroPhaseEventRequested.length; i < j; i += 1) {
-            event = new CustomEvent(this.zeroPhaseEventRequested[i], {
-                detail: that.frequency.value
-            });
-            this.context.dispatchEvent(event);
+    start () {
+        if (!this.paused) {
+            this.resetPhase();
         }
-        this.zeroPhaseEventRequested = false;
+        this.generator.onaudioprocess = this.handleAudioProcess;
+        // this.generator.onaudioprocess = this.getAudioProcessHandler(this);
+        //    this.generator.addEventListener("audioprocess", this.getGenerator(this));
     }
-};
-IdealOscillator.prototype.resetPhase = function () {
-    this.phase = 0;
-    this.sampleAndHoldBuffer.phase = 0;
-    this.zeroPhaseActions();
-};
-IdealOscillator.prototype.getComputedFrequency = function (frequency, detune) {
-    return frequency * Math.pow(2, detune / 1200);
-};
-IdealOscillator.prototype.connect = function (node) {
-    if (node.hasOwnProperty("input")) {
-        this.generator.connect(node.input);
-    } else {
-        this.generator.connect(node);
-    }
-};
-IdealOscillator.prototype.start = function () {
-    if (!this.paused) {
-        this.resetPhase();
-    }
-    this.generator.onaudioprocess = this.getGenerator(this);
-    //    this.generator.addEventListener("audioprocess", this.getGenerator(this));
-};
-IdealOscillator.prototype.stop = function () {
-    this.generator.onaudioprocess = null;
+
+    stop () {
+        this.generator.onaudioprocess = null;
 
     //    this.generator.removeEventListener("audioprocess");
-};
-IdealOscillator.prototype.setWaveform = function (waveformName) {
-    if (typeof IdealOscillator.waveforms[waveformName] === "function") {
-        this.selectedWaveform = IdealOscillator.waveforms[waveformName];
     }
-};
-IdealOscillator.prototype.destroy = function destroyIdealOscillator () {
-    let i, j, def;
-    this.dc.destroy();
-    this.dc = null;
 
-    for (i = 0, j = this.inputDefs.length; i < j; i += 1) {
-        def = this.inputDefs[i];
-        this[def.name + "Node"].disconnect();
-        this[def.name] = null;
-        this[def.name + "Node"] = null;
+    getIncrementedPhase (frequency) {
+        let increment = frequency / this.context.sampleRate;
+        this.phase += increment;
+        if (this.phase > 1) {
+            while (this.phase > 1) {
+                this.phase -= 1;
+            }
+            this.zeroPhaseActions();
+        }
+        return this.phase;
     }
-    this.mergedInput.disconnect();
-    this.mergedInput = null;
 
-    this.generator.onaudioprocess = null;
-    this.generator.disconnect();
-    this.generator = null;
+    getComputedFrequency (frequency, detune) {
+        return frequency * Math.pow(2, detune / 1200);
+    }
 
-};
+    requestZeroPhaseEvent (eventName) {
+
+        if (!this.zeroPhaseEventRequested) {
+            this.zeroPhaseEventRequested = [];
+        }
+        if (this.zeroPhaseEventRequested.indexOf(eventName) === -1) {
+            this.zeroPhaseEventRequested.push(eventName);
+        }
+    }
+
+    zeroPhaseActions () {
+        if (this.zeroPhaseEventRequested) {
+            this.zeroPhaseEventRequested.forEach((evt, i) => {
+                const event = new CustomEvent(evt, {
+                    detail: this.frequency.value
+                });
+                this.context.dispatchEvent(event);
+            });
+            this.zeroPhaseEventRequested = false;
+        }
+    }
+
+    resetPhase () {
+        this.phase = 0;
+        this.sampleAndHoldBuffer.phase = 0;
+        this.zeroPhaseActions();
+    }
+
+    connect (node) {
+        if (node.hasOwnProperty("input")) {
+            this.generator.connect(node.input);
+        } else {
+            this.generator.connect(node);
+        }
+    }
+
+    setWaveform (waveformName) {
+        if (typeof IdealOscillator.waveforms[waveformName] === "function") {
+            this.selectedWaveform = IdealOscillator.waveforms[waveformName];
+        }
+    }
+
+    destroy () {
+        this.generator.onaudioprocess = null;
+        this.dc = null;
+
+        this.inputDefs.forEach((def, i) => {
+            this[def.name + "Node"].disconnect();
+            this[def.name] = null;
+            this[def.name + "Node"] = null;
+        });
+
+        this.mergedInput.disconnect();
+        this.mergedInput = null;
+
+        this.generator.disconnect();
+        this.generator = null;
+    }
+}
+IdealOscillator.waveforms = waveforms;
 export default IdealOscillator;
